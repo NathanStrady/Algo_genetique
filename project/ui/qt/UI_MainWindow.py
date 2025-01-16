47132df98d53f316e986b48c7ed59f231190e44f
Nathan Strady <145336391+NathanStrady@users.noreply.github.com>
2025-01-16 19:40:03 +0100

    Corrections syntaxiques

--- project/ui/qt/UI_MainWindow.py
+++ project/ui/qt/UI_MainWindow.py
@@ -120,7 +120,7 @@ class Ui_MainWindow(object):
         self.choixDeLaStratGieDeGNRationDeDPartLabel.setText(_translate("MainWindow", "Choix de la stratégie d\'initialisation :"))
         self.choixDeLaStratGieDeGNRationDeDPartComboBox.setItemText(0, _translate("MainWindow", "Aléatoire"))
         self.choixDeLaStratGieDeGNRationDeDPartComboBox.setItemText(1, _translate("MainWindow", "Algorithme Glouton du plus proche voisin"))
-        self.choixDeLaStratGieLabel.setText(_translate("MainWindow", "Choix de la stratégie"))
+        self.choixDeLaStratGieLabel.setText(_translate("MainWindow", "Choix de la stratégie séléction :"))
         self.choixDeLaStratGieComboBox.setItemText(0, _translate("MainWindow", "Sélection par tournois"))
         self.choixDeLaStratGieComboBox.setItemText(1, _translate("MainWindow", "Sélection par rang"))
         self.pushButton.setText(_translate("MainWindow", "Lancer"))
